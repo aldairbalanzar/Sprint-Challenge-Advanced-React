083fb5f61dcf1e5508bd2a74c56f7cce6cf6bdf4
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-03-13 14:23:14 -0400

    updated testing for card component

--- client/src/Card.test.js
+++ client/src/Card.test.js
@@ -1,15 +1,15 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { render } from "@testing-library/react"
-// import Card from './Card';
-// import "jest-dom/extend-expect";
-// import { fetchPosts } from 'http://localhost:5000/api/players';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react"
+import Card from './Card';
+import "jest-dom/extend-expect";
+import { fetchPosts } from 'http://localhost:5000/api/players';
 
-// jest.mock('http://localhost:5000/api/players');
+jest.mock('http://localhost:5000/api/players');
 
-// test("list of players", () => {
-//     expect(fetchPosts).toHaveBeenCalled(1);
-// })
+test("list of players", async () => {
+    expect(fetchPosts).toHaveBeenCalled(1);
+})
 
 // test("my app render test", () => {
 //     render(<Card />);
